896db3020d5f76464500adf6ca979a08523835cc
Geordan Bolden <gbolden101@gmail.com>
2026-09-17 18:59:50 -0400

    vignette dev

--- Project_1/bulkanalysis/R/edgeR_diffexp.R
+++ Project_1/bulkanalysis/R/edgeR_diffexp.R
@@ -40,7 +40,7 @@
 #'
 #' OUTPUTS:
 #'
-#' A list of data frames containing every possible combination of binary
+#' A list of data frames containing each possible combination of binary
 #' comparisons (control vs. experimental group) within the supplied
 #' metadata.
 #'
@@ -75,12 +75,15 @@ edgeR_diffexp <- function(dds_object,
   #0-Intercept
 
   #Converting to a DGEList
+  #Extracting the raw counts and metadata
   raw_counts <- dds_object@assays@data$counts
   metadata <- dds_object@colData
   y <- DGEList(counts = raw_counts , group = metadata$condition)
 
   #Filter by low counts
+  #Determine which genes in the count matrix that have sufficiently large counts to be retained in a statistical analysis
   keep <- filterByExpr(y)
+  #enter those gene in count matrix
   y <- y[keep, , keep.lib.sizes=FALSE]
   #Performs TMM Normalization, Corrects for systemic biases between samples.
   y <- normLibSizes(y)
@@ -95,15 +98,24 @@ edgeR_diffexp <- function(dds_object,
   # Always use a model intecept term because then the data will no fit to the glm line as closely
   # A model without an intercept term would only be recommended in cases where there is a strong biological reason why a zero covariate should be associated with a zero expression value (Law, et.al., F10000Research, 2020, PMID: 33604029)
 
+  #covariate: numerical variable
+  #factor: catagorical variable
+
+  #Why set a 0 intercept for differential expression?
+  #Design matrix purpose: Defines the form of the model, the relationship between genes and explanatory variable(the disease state)
+  # If a variable is a factor, then models with and without an intercept are equivalent, but if variable is a covariate then the models are fundamentally different
+  #https://www.bioconductor.org/packages//release/workflows/vignettes/RNAseq123/inst/doc/designmatrices.html
   design <- model.matrix(~0+condition, data = y$samples)
   conds <- gsub("^condition", "", colnames(design))
 
+  #set the combination of pairwise contrasts
   contrast_strings <- utils::combn(conds, 2, FUN = function(x) paste0(x[1], "-", x[2]))
 
-
+  #set contrasts from colnames of the design matrix for each pairwise combination
   contrast_strings
   colnames(design) <- gsub(glue("^{condition_col}"), "", colnames(design))
 
+  #Constructs the contrast matrices corresponding to the factor parameters
   my.contrasts <- makeContrasts(
     contrasts = contrast_strings,
     levels = colnames(design))
@@ -112,10 +124,11 @@ edgeR_diffexp <- function(dds_object,
   results <- list()
 
   # Run Dispersion on the Data
+  #Calculate
   y <- estimateDisp(y, design)
 
   #fit model once
-  #
+  #generalize QuaziLikelihood design
   fit <- glmQLFit(y, design)
 
   # run the test for each contrast
